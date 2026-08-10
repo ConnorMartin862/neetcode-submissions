c90b49512df0d6bf60e9c11c8b543c7e703395a1
Connor Martin <connormm@umich.edu>
2026-08-10 11:32:57 -0400

    Add: spiral-matrix - submission-3

---
+++ Data Structures & Algorithms/spiral-matrix/submission-3.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        vector<int> results(matrix.size() * matrix[0].size(), 0);
+        int index = 0;
+        int left = 0;
+        int right = matrix[0].size() - 1;
+        int top = 0;
+        int bottom = matrix.size() - 1;
+        while (left <= right && top <= bottom) {
+            for (int i = left; i <= right; i++) {
+                results[index] = matrix[top][i];
+                index++;
+            }
+            top++;
+            if (bottom < top) {break;}
+            for (int i = top; i <= bottom; i++) {
+                results[index] = matrix[i][right];
+                index++;
+            }
+            right--;
+            if (right < left) {break;}
+            for (int i = right; i >= left; i--) {
+                results[index] = matrix[bottom][i];
+                index++;
+            }
+            bottom--;
+            if (bottom < top) {break;}
+            for (int i = bottom; i >= top; i--) {
+                results[index] = matrix[i][left];
+                index++;
+            }
+            left++;
+        }
+        return results;
+    }
+};
