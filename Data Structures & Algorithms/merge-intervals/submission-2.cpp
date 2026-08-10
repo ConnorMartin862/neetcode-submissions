6f4d27c5f161224d1e356c2a9aef0b238be1f826
Connor Martin <connormm@umich.edu>
2026-08-10 11:53:27 -0400

    Add: merge-intervals - submission-2

---
+++ Data Structures & Algorithms/merge-intervals/submission-2.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end());
+        vector<vector<int>> results;
+        results.push_back(intervals[0]);
+        for (int i = 1; i < intervals.size(); i++) {
+            if (intervals[i][0] > results.back()[1]) {
+                results.push_back(intervals[i]);
+            } else {
+                results.back()[1] = max(results.back()[1], intervals[i][1]);
+            }
+        }
+
+        return results;
+    }
+};
